6e17d2fcf6ab2f7318773c9e1f514bfea4c93461
SecondKnife <96180954+SecondKnife@users.noreply.github.com>
2025-02-26 19:57:01 +0700

    blank

--- src/App.tsx
+++ src/App.tsx
@@ -103,7 +103,7 @@ export default function BirthdayLanding() {
                   src={src}
                   style={{
                     width: "100%",
-                    height: "100%",
+                    height: "90%",
                     borderRadius: "10px",
                     boxShadow: "0px 4px 10px rgba(0,0,0,0.2)",
                   }}
