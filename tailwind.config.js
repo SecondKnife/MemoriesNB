6940360af1b2a682d06e9e83db33d4d734a95431
SecondKnife <96180954+SecondKnife@users.noreply.github.com>
2025-02-26 19:41:49 +0700

    staged

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"], // Chỉ định nơi Tailwind cần quét class
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
