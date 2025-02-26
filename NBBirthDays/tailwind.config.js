43aec6ae0af6c2eb683eec94cd1d1b29fb29d4fc
SecondKnife <96180954+SecondKnife@users.noreply.github.com>
2025-02-26 19:51:39 +0700

    clean code

--- NBBirthDays/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"], // Chỉ định nơi Tailwind cần quét class
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  };
-  
